525af7c6096c4228e119ad0fc85250757d5041d3
CodeBerry <codeberry@Air-Aleksander.Dlink>
2021-12-17 12:53:55 +0100

    Adding the ability to select a finished task.

--- js/index.js
+++ js/index.js
@@ -12,12 +12,17 @@ push.addEventListener("click", () => {
             <div class="task">
                 <span id="taskname">
                     ${newTaskInput.value}
+                    ${tasks.style.textDecoration = ""}
                 </span>
                 <button class="delete">
                     <i class="far fa-trash-alt"></i>
                 </button>
             </div>
         `;
+
+        tasks.addEventListener("click", () => {
+            tasks.style.textDecoration = "line-through";
+        })
 
         const current_tasks = document.querySelectorAll(".delete");
         for(var i=0; i<current_tasks.length; i++){

b1ac880c8d269fa3e796069b41e25223273a5eee
CodeBerry <codeberry@Air-Aleksander.Dlink>
2021-12-20 13:32:23 +0100

    Update v1.0

--- js/index.js
+++ js/index.js
@@ -1,25 +1,42 @@
-const tasks = document.querySelector("#tasks");
-const btn = document.querySelector("#add");
-const input = document.querySelector("#newtask input");
+const push = document.querySelector("#push");
+const newTaskInput = document.querySelector("#newtask input");
+const tasksList = document.querySelector("#tasks");
 
-// event listeners
-add.addEventListener("click", () => {
-  if(input.value.length == 0) {
-    alert("Please enter a task!")
-  }
-  else {
-    addToDo();
-    input.value = "";
-  }
-})
+const createTask = (value, onClick, onDelete) => {
+    const task = document.createElement('div');
+    task.className = "task";
+    task.onclick = () => onClick(task)
 
-//functions
-function addToDo() {
-  const newElement = document.createElement("li");  
-  const newItem = document.createTextNode(input.value);  
- 
-  newElement.appendChild(newItem);
-  tasks.appendChild(newElement);
+    const span = document.createElement('span');
+    span.id = "taskname";
+    span.innerText = value;
+
+    const button = document.createElement('button');
+    button.className = "delete"
+    button.onclick = () => onDelete(task);
+
+    const i = document.createElement('i');
+    i.classList = ['far fa-trash-alt']
+    button.appendChild(i);
+    
+    task.appendChild(span);
+    task.appendChild(button);
+
+    return task;
 }
 
+const onClick = () => {
+    const newTaskValue = newTaskInput.value;
+    if(newTaskValue.length === 0) {
+        alert("Please Enter a Task")
+        return;
+    }
+    const taskOnClick = (task) => task.classList.toggle('completed');
+    const taskOnDelete = (task) => tasksList.removeChild(task);
 
+    const newTask = createTask(newTaskValue, taskOnClick, taskOnDelete);
+    tasksList.appendChild(newTask);
+    newTaskInput.value = "";
+}
+
+push.addEventListener("click", onClick)
